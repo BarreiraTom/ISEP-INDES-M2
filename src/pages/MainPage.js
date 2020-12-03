029abc2e37d15f8f2dd20c95ab9d4dc4a40227b3
JstTom <1200438@isep.ipp.pt>
2020-12-03 18:51:06 +0000

    Updated mainPage to make components work with eachother

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import MainVid from '../components/mainVid/mainVid'
+import MiniatureVids from '../components/miniatureVid/miniatureVid'
 
 export default function Page(props) {
+
+  const [sources, setSources] = useState([{
+      id: "1",
+      name: "rocketleague",
+      desc: "rocketleague",
+      type: "twitch"
+  },{
+      id: "2",
+      name: "ipCam",
+      desc: "192.168.137.101:8080/video",
+      type: "ipCam"
+  },{
+      id: "3",
+      name: "Youtube-vid1",
+      desc: "https://www.youtube.com/watch?v=LXb3EKWsInQ",
+      type: "youtube"
+  },{
+    id:"4",
+    name:"LocalCamera",
+    desc:"VYRMPfKvZ2ogcK7tcVBgsbgwwGnastO3xo46",
+    type:"webcam"
+  }]);
+
+  const [mainSource, setMainSource] = useState({
+      type: "none"
+  })
+
+  const changeLiveVid = (src, el) =>{
+        el.preventDefault();
+        setMainSource(src);
+  }
+
   return (
     <>
-        <MainVid/>
+        <MainVid mainSource={mainSource}/>
+        <MiniatureVids sources={sources} changeLiveVid={changeLiveVid} mainSource={mainSource}/>
     </>
   );
 }
