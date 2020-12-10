47ca862eb6f6bad31da82d782d323ae8e891bc16
JstTom <1200438@isep.ipp.pt>
2020-12-10 20:12:41 +0000

    Added beggining of rightHand menu

---
+++ src/components/rightMenu/rightMenu.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+import './rightMenu.css';
+
+export default function rightMenu(props) {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
